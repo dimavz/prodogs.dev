bbe70551fad1d552838aff811fd790a69e9273ef
dimavz <zatulenko@gmail.com>
2018-12-31 13:55:52 +0300

    Переменная $obj заменена на $this

--- components/com_cobalt/views/records/tmpl/default_list_myblog_with_tabs.php
+++ components/com_cobalt/views/records/tmpl/default_list_myblog_with_tabs.php
@@ -2,19 +2,11 @@
 defined('_JEXEC') or die('Restricted access');
 $i = $n = 0;
 $params = $this->tmpl_params['list'];
+
 foreach ($this->items as $item)
 {
 	?>
-	<style>
-	.dl-horizontal dd {
-		margin-bottom: 10px;
-	}
-
-.line-brk {
-	margin-left: 0px !important;
-}
-</style>
-
+	
 	<article class="has-context<?php if($item->featured) {echo ' featured';}?>">
 				<div class="pull-right controls">
 					<div class="btn-group" style="display: none;">
@@ -155,7 +147,118 @@ foreach ($this->items as $item)
 	}
 
 	echo "</div>"; // end class='tabbable'
+
+	// Вывод мета инфы
+
+	$category = array();
+	$author = array();
+	$details = array();
+
+	if($params->get('tmpl_core.item_categories') && $item->categories_links)
+				{
+					$category[] = sprintf('%s: %s', (count($item->categories_links) > 1 ? JText::_('CCATEGORIES') : JText::_('CCATEGORY')), implode(', ', $item->categories_links));
+				}
+				if($params->get('tmpl_core.item_user_categories') && $item->ucatid)
+				{
+					$category[] = sprintf('%s: %s', JText::_('CUSERCAT'), $item->ucatname_link);
+				}
+				if($params->get('tmpl_core.item_author') && $item->user_id)
+				{
+					$author[] = JText::sprintf('CWRITTENBY', CCommunityHelper::getName($item->user_id, $this->section));
+				}
+				if($params->get('tmpl_core.item_author_filter'))
+				{
+					$author[] = FilterHelper::filterButton('filter_user', $item->user_id, NULL, JText::sprintf('CSHOWALLUSERREC', CCommunityHelper::getName($item->user_id, $this->section, array('nohtml' => 1))), $this->section);
+				}
+				if($params->get('tmpl_core.item_ctime'))
+				{
+					$author[] = JText::sprintf('CONDATE', JHtml::_('date', $item->created, $params->get('tmpl_core.item_time_format')));
+				}
+
+				if($params->get('tmpl_core.item_mtime'))
+				{
+					$author[] = sprintf('%s: %s', JText::_('CCHANGEON'), JHtml::_('date', $item->modify, $params->get('tmpl_core.item_time_format')));
+				}
+
+				if($params->get('tmpl_core.item_extime'))
+				{
+					$author[] = sprintf('%s: %s', JText::_('CEXPIREON'), $item->expire ? JHtml::_('date', $item->expire, $params->get('tmpl_core.item_time_format')) : JText::_('CNEVER'));
+				}
+
+				if($params->get('tmpl_core.item_type'))
+				{
+					$details[] = sprintf('%s: %s %s', JText::_('CTYPE'), $item->type_name, ($params->get('tmpl_core.item_type_filter') ? FilterHelper::filterButton('filter_type', $item->type_id, NULL, JText::sprintf('CSHOWALLTYPEREC', $item->type_name), $this->section) : NULL));
+				}
+				if($params->get('tmpl_core.item_hits'))
+				{
+					$details[] = sprintf('%s: %s', JText::_('CHITS'), $item->hits);
+				}
+				if($params->get('tmpl_core.item_comments_num'))
+				{
+					$details[] = sprintf('%s: %s', JText::_('CCOMMENTS'), CommentHelper::numComments($this->submission_types[$item->type_id], $item));
+				}
+				if($params->get('tmpl_core.item_vote_num'))
+				{
+					$details[] = sprintf('%s: %s', JText::_('CVOTES'), $item->votes);
+				}
+				if($params->get('tmpl_core.item_favorite_num'))
+				{
+					$details[] = sprintf('%s: %s', JText::_('CFAVORITED'), $item->favorite_num);
+				}
+				if($params->get('tmpl_core.item_follow_num'))
+				{
+					$details[] = sprintf('%s: %s', JText::_('CFOLLOWERS'), $item->subscriptions_num);
+				}
+				?>
+
+				<?php if($params->get('tmpl_core.item_readon')): ?>
+					<p>
+						<a class="btn btn-primary" href="<?php echo JRoute::_($item->url);?>"><?php echo JText::_('CREADMORE'); ?></a>
+					</p>
+				<?php endif;?>
+
+				<?php if($category || $author || $details): ?>
+					<div class="clearfix"></div>
+
+					<div class="well well-small">
+						<?php if($params->get('tmpl_core.item_author_avatar')):?>
+							<div class="pull-right">
+								<img class="img-polaroid" src="<?php echo CCommunityHelper::getAvatar($item->user_id, $params->get('tmpl_core.item_author_avatar_width', 40), $params->get('tmpl_core.item_author_avatar_height', 40));?>" />
+							</div>
+						<?php endif;?>
+						<small>
+							<dl>
+								<?php if($category):?>
+									<dt><?php echo JText::_('CCATEGORY');?></dt>
+									<dd><?php echo implode(' ', $category);?></dd>
+								<?php endif;?>
+								<?php if($author):?>
+									<dt><?php echo JText::_('Posted');?></dt>
+									<dd>
+										<?php echo implode(', ', $author);?>
+									</dd>
+								<?php endif;?>
+								<?php if($details):?>
+									<dt>Info</dt>
+									<dd class="hits">
+										<?php echo implode(', ', $details);?>
+									</dd>
+								<?php endif;?>
+							</dl>
+						</small>
+					</div>
+				<?php endif;?>
+				<?php
 	echo '</article>';
 }
 ?>
 
+<style>
+	.dl-horizontal dd {
+		margin-bottom: 10px;
+	}
+
+.line-brk {
+	margin-left: 0px !important;
+}
+</style>
